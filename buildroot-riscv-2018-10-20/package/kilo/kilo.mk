79c9758a4c88e3bcadeda6450b50d5e23fb701ad
ztex <ztex030640417@gmail.com>
2019-04-02 01:07:46 +0800

    Fix Now git clone has been executed as expect

--- buildroot-riscv-2018-10-20/package/kilo/kilo.mk
+++ buildroot-riscv-2018-10-20/package/kilo/kilo.mk
@@ -1,10 +1,14 @@
 KILO_VERSION = 62b099af00b542bdb08471058d527af258a349cf
-KILO_SOURCE = git://github.com:antirez/kilo.git
+# KILO_SOURCE = git://github.com/antirez/kilo.git
+# FOO_SITE = $(call github,<user>,<package>,$(FOO_VERSION))
+# KILO_SITE = $(call github, antirez, kilo,$(FOO_VERSION))
+KILO_SITE = git://github.com/antirez/kilo.git
+# KILO_SOURCE = 
 KILO_SITE_METHOD = git
 
 # $(@D): the path to the build directory
 define KILO_BUILD_CMDS
-	$(MAKE) -C $(@D)
+	$(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)
 endef
 
 # See https://stackoverflow.com/questions/8014991/how-do-i-add-a-a-package-to-buildroot-which-is-available-in-a-git-repository
